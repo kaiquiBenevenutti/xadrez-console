2fadd87321910b9c91c80353e596d07c2119eb1d
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-07-25 11:11:23 -0300

    Tratando erros com excecao personalizada

--- xadres-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ xadres-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5ca9201b8fcce73d0d32ec581c60d290bacda8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a41365e1d4f407215ec449f16326a05d392d788")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

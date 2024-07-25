0a41365e1d4f407215ec449f16326a05d392d788
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-07-25 10:44:44 -0300

    Colocando a colocar peças no tabuleiro

--- xadres-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ xadres-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bd4aebcb1ae106c1b5f2fad1a0c9187e14dbc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5ca9201b8fcce73d0d32ec581c60d290bacda8")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
